853197e12014aef64034fbb7dd8a005c2fc0c8d9
MathiTarazonad <vladitarazona@gmail.com>
2023-01-02 14:20:52 -0300

    Changed images name and update the name where necessary

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import gameImage from '../images/human.svg';
 
 export default function Menu() {
   return (
     <div className='main-menu'>
       <h1>Country Quiz</h1>
-      <img src='../images/undraw_adventure_4hum 1.svg' alt='gameImage' className='menu-img'/>
+      <img src={gameImage} alt='gameImage' className='menu-img'/>
       <h2>Actual Record: 10</h2>
       <button className='play-btn'>Play</button>
     </div>
