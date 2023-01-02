63aeb5dbb1d1af554f997406592299a3629625e2
MathiTarazonad <vladitarazona@gmail.com>
2023-01-02 15:05:10 -0300

    Added functionality to start playing

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import gameImage from '../images/human.svg';
+import { useDispatch, useSelector } from 'react-redux';
+import { setIfIsPlaying } from '../store/gameSlice';
 
 export default function Menu() {
+  const dispatch = useDispatch()
+  const bestStreak = useSelector(store => store.game.bestStreak);
+  
+  const handlePlay = () => {
+    dispatch(setIfIsPlaying());
+  }
+
   return (
     <div className='main-menu'>
       <h1>Country Quiz</h1>
       <img src={gameImage} alt='gameImage' className='menu-img'/>
-      <h2>Actual Record: 10</h2>
-      <button className='play-btn'>Play</button>
+      <h2>Actual Record: {bestStreak}</h2>
+      <button className='play-btn' onClick={() => handlePlay()}>Play</button>
     </div>
   )
 }
