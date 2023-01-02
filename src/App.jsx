85c051aedefc23ca8faea232c0ca772dfd3d179f
MathiTarazonad <vladitarazona@gmail.com>
2023-01-02 15:04:19 -0300

    Render the game or menu based on isPlaying state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import Game from "./components/Game"
-import Menu from "./components/Menu"
+import Game from './components/Game';
+import Menu from './components/Menu';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const isPlaying = useSelector(store => store.game.isPlaying);
   return (
-    <div className="App">
-      <Menu />
-      <Game />
+    <div className='App'>
+      {isPlaying ? <Game /> : <Menu />}
       <footer>
-        Challenge by <a href="https://devchallenges.io/challenges/Bu3G2irnaXmfwQ8sZkw8#" target="_blank" rel='noopener noreferrer' >DevChallenges</a>. Coded by <a href="https://github.com/Mathitarazonad" target='_blank' rel='noopener noreferrer' >Mathías Tarazona</a>.
+        Challenge by <a href='https://devchallenges.io/challenges/Bu3G2irnaXmfwQ8sZkw8#' target='_blank' rel='noopener noreferrer' >DevChallenges</a>. Coded by <a href='https://github.com/Mathitarazonad' target='_blank' rel='noopener noreferrer' >Mathías Tarazona</a>.
       </footer>
     </div>
   )
