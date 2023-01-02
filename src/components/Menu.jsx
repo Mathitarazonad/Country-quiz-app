734972a362620272b3b326d8590316caf4e873ce
MathiTarazonad <vladitarazona@gmail.com>
2023-01-02 11:24:47 -0300

    Created menu component

---
+++ src/components/Menu.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Menu() {
+  return (
+    <div className='main-menu'>
+      <h1>Country Quiz</h1>
+      <img src='../images/undraw_adventure_4hum 1.svg' alt='gameImage' className='menu-img'/>
+      <h2>Actual Record: 10</h2>
+      <button className='play-btn'>Play</button>
+    </div>
+  )
+}
