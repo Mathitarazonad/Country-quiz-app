b8ba59901b9c1fbcdb0d613830739f5cc5f0faa8
MathiTarazonad <vladitarazona@gmail.com>
2023-01-03 14:58:26 -0300

    Added streaks component

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import GamePlaying from './GamePlaying'
+import React from 'react';
+import Streaks from './Streaks';
+import GamePlaying from './GamePlaying';
 
 export default function Game() {
   return (
     <div className='game'>
       <h2>Country Quiz</h2>
       <GamePlaying />
+      <Streaks />
     </div>
   )
 }
