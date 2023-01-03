1cc752874d5b146aa264af1e217644ba0db9268b
MathiTarazonad <vladitarazona@gmail.com>
2023-01-03 15:11:21 -0300

    Added useEffect hooks in both components to render when user decides to play and when user clicks on next

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import Streaks from './Streaks';
 import GamePlaying from './GamePlaying';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setQuiz } from '../store/gameSlice';
+import { getQuizSetup } from '../functions';
 
 export default function Game() {
+  const dispatch = useDispatch();
+  const {allCountries} = useSelector(store => store.game);
+
+  useEffect(() => {
+    const {reference, answer, options, questionOption: question} = getQuizSetup(allCountries);
+    dispatch(setQuiz({reference, answer, options, question}));
+  }, []);
+
   return (
     <div className='game'>
       <h2>Country Quiz</h2>
--- src/components/GamePlaying.jsx
+++ src/components/GamePlaying.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Options from './Options';
 import gameImage from '../images/human.svg';
+import { useSelector, useDispatch } from 'react-redux';
+import { setIfUserHasChosen, setQuiz } from '../store/gameSlice';
+import { getQuizSetup } from '../functions';
 
 export default function GamePlaying() {
+  const dispatch = useDispatch();
+  const {userHasChosen, allCountries} = useSelector(store => store.game);
+
+  const handleNext = () => {
+    dispatch(setIfUserHasChosen());
+    const {reference, answer, options, questionOption: question} = getQuizSetup(allCountries);
+    dispatch(setQuiz({reference, answer, options, question}));
+  }
+
   return (
     <div className='game-container'>
       <img src={gameImage} alt='gameImg' />
       <p>Asuncion is the capital of</p>
       <Options />
+      {userHasChosen && <button className='next-btn' onClick={() => handleNext}>Next</button>}
     </div>
   )
 }
