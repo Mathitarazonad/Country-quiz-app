ca539741e5988dd1d3e2c215c05c4da85231b81a
MathiTarazonad <vladitarazona@gmail.com>
2023-01-06 09:34:34 -0300

    Reset the counter when user clicks on next button

--- src/components/NextButton.jsx
+++ src/components/NextButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { setIfUserHasChosen, setChoices, setQuiz, setIfIsWinning, setStop } from '../store/gameSlice';
+import { setIfUserHasChosen, setChoices, setQuiz, setIfIsWinning, setStop, setTimeOut } from '../store/gameSlice';
 import { getQuizSetup } from '../functions';
 import nextMp3 from '../sounds/next.mp3';
 import resultsMp3 from '../sounds/result.mp3';
@@ -14,10 +14,12 @@ export default function NextButton() {
     dispatch(setChoices({correct:undefined, incorrect:undefined}));
     const {reference, answer, options, questionOption: question} = getQuizSetup(allCountries);
     dispatch(setQuiz({reference, answer, options, question}));
+    dispatch(setTimeOut(10));
+
     if (stop) {
       dispatch(setIfIsWinning(false));
       dispatch(setStop());
-      new Audio(resultsMp3).play();
+      new Audio(resultsMp3).play();  
     } else {
       new Audio(nextMp3).play();
     }
