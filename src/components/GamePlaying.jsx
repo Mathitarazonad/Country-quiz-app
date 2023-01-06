96df9ab5c146fcae966b818553c38a4bddc6d6cb
MathiTarazonad <vladitarazona@gmail.com>
2023-01-06 09:29:16 -0300

    Added counter component

--- src/components/GamePlaying.jsx
+++ src/components/GamePlaying.jsx
@@ -4,6 +4,7 @@ import gameImage from '../images/human.svg';
 import Question from './Question';
 import NextButton from './NextButton';
 import { useSelector } from 'react-redux';
+import Counter from './Counter';
 
 export default function GamePlaying() {
   const {userHasChosen, currentStreak} = useSelector(store => store.game);
@@ -15,6 +16,7 @@ export default function GamePlaying() {
       <Options />
       <div className='bottom-section'>
         <p className='current-streak'>Streak: {currentStreak}</p>
+        <Counter />
         {userHasChosen && <NextButton />}
       </div>
     </div>
