c8c2e9a164daf3ef88a74f76100fa87c6f7d25cb
MathiTarazonad <vladitarazona@gmail.com>
2023-01-06 10:55:02 -0300

    Added sound when user cannot go back to the menu and used the animation when it is no possible to do it

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {ImArrowLeft} from 'react-icons/im';
 import { useDispatch, useSelector } from 'react-redux';
 import { setIfIsPlaying } from '../store/gameSlice';
 import backMp3 from '../sounds/back.mp3';
+import invalidMp3 from '../sounds/denie-96177.mp3';
 
 export default function BackButton() {
   const dispatch = useDispatch();
-  const {userHasChosen} = useSelector(store => store.game);
+  const {userHasChosen, isWinning} = useSelector(store => store.game);
+  const [invalidBtn, setInvalidBtn] = useState(false);
 
   const handleBack = () => {
-    if (userHasChosen) { 
+    if (userHasChosen || !isWinning) { 
       dispatch(setIfIsPlaying());
       new Audio(backMp3).play();
+    } else {
+      new Audio(invalidMp3).play();
+      setInvalidBtn(true);
     }
   }
 
+  useEffect(() => {
+    if (invalidBtn) {
+      setTimeout(() => setInvalidBtn(false), 1000)
+    }
+  })
+
   return (
-      <ImArrowLeft className='back-btn' onClick={() => handleBack()}/>
+      <ImArrowLeft className='back-btn' onClick={() => handleBack()} style={invalidBtn && {animation: 'shuffle .5s linear both'}}/>
   )
 }
